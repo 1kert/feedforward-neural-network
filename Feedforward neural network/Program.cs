a4276119c9e7aba8a896db0d96448de00d63fab0
Kert Mänd <kertmand1@gmail.com>
2025-11-15 14:53:49 +0200

    added convergence test

--- Feedforward neural network/Program.cs
+++ Feedforward neural network/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        
+        Network network = new(2, 2, 2);
+        Datapoint datapoint = new([0.2, 0.9], [1, 0]);
+
+        for (int i = 0; i < 100_000; i++)
+        {
+            double[] output = network.Calculate(datapoint.Inputs);
+            Console.WriteLine($"[{string.Join(", ", output)}]");
+            network.Learn(datapoint, 0.2);
+        }
     }
 }
