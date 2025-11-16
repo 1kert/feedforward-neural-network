df1dc387acb26e44ab144189ffa2ad5b1c89f969
Kert Mänd <kertmand1@gmail.com>
2025-11-16 20:22:58 +0200

    expanded tests

--- Feedforward neural network/Program.cs
+++ Feedforward neural network/Program.cs
@@ -4,14 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Network network = new(2, 2, 2);
-        Datapoint datapoint = new([0.2, 0.9], [1, 0]);
+        Network network = new(2, 8, 8, 4, 2); // todo: fix
+        Datapoint[] dataPoints =
+        [
+            new([0.2, 0.3], [1, 0]),
+            new([0.3, 0.4], [0.5, 0.5]),
+            new([0.4, 0.5], [0, 0])
+        ];
+        
+        const double learningRate = 0.02;
 
         for (int i = 0; i < 100_000; i++)
         {
-            double[] output = network.Calculate(datapoint.Inputs);
-            Console.WriteLine($"[{string.Join(", ", output)}]");
-            network.Learn(datapoint, 0.2);
+            foreach (Datapoint datapoint in dataPoints)
+            {
+                network.Learn(datapoint, learningRate);
+            }
+
+            // double[] output = network.Calculate(datapoint.Inputs);
+            // network.Learn(datapoint, 0.2);
+        }
+        
+        foreach (Datapoint datapoint in dataPoints)
+        {
+            Console.WriteLine($"[{string.Join(", ", network.Calculate(datapoint.Inputs))}]");
         }
     }
 }
